d0ff1be7af367c31ec62553e470ec12e808851de
TRSandwich <rluke2011@proton.me>
2023-09-25 18:25:51 +0100

    JS and TS added

---
+++ HelloScript/HelloJS/HelloWorldJS.js
@@ -0,0 +1,16 @@
+console.log('Hello World');
+keepConsoleOpen()
+
+function keepConsoleOpen() {
+    const readline = require ('readline');
+
+    const readLineInterface = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+    });
+
+    readLineInterface.question('Press enter to exit', () => {
+        readLineInterface.close();
+        process.exit();
+    });
+}
